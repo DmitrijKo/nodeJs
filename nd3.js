048b6405845b2633a33ceb4def4cddbad87f1da3
DmitrijKo <kdimka666@gmail.com>
2021-07-18 22:05:43 +0300

    nx

--- nd3.js
+++ nd3.js
@@ -1,10 +1,31 @@
 var m = [1, 2, 3, 4, 5];
 var rez1 = new Array(m.length);
 for (let i = 0; i < rez1.length; i++) {
-rez1[i] = new Array(m.length);
+   rez1[i] = new Array(m.length);
 }
+
+//for (let i = 0; i < rez1.length; i++) {
+//   rez1[i] = m;
+//   for (let j = 0; j < m.length; j++) {
+//      m.push(m[0]);
+//      m.shift();
+//      console.log(m);
+//      break;
+//   }
+//   console.log(rez1);
+//}
+
+for (let i = 0; i < rez1.length; i++) {
+   rez1[i] = m;
+   m.push(m[0]);
+   m.shift();
+   console.log(m);
+   console.log(rez1);
+}
+
+
 // ND
-console.log(rez1);
+//console.log(rez1);
 /*
 1, 2, 3, 4, 5
 2, 3, 4, 5, 1
@@ -12,27 +33,53 @@ console.log(rez1);
 4, 5, 1, 2, 3
 5, 1, 2, 3, 4
 */
-var rez2 = new Array(m.length);
-for (let i = 0; i < rez2.length; i++) {
-rez2[i] = new Array(m.length);
-}
-// ND
-console.log(rez2);
-/*
-1, 2, 3, 4, 5
-5, 1, 2, 3, 4
-4, 5, 1, 2, 3
-3, 4, 5, 1, 2
-2, 3, 4, 5, 1
-*/
+//var rez2 = new Array(m.length);
+//for (let i = 0; i < rez2.length; i++) {
+//rez2[i] = new Array(m.length);
+//}
+//// ND
+//console.log(rez2);
+///*
+//1, 2, 3, 4, 5
+//5, 1, 2, 3, 4
+//4, 5, 1, 2, 3
+//3, 4, 5, 1, 2
+//2, 3, 4, 5, 1
+//*/
 
 
 
 ///
+/*
+i = 0
+reikiaSurusiuoti.length = 12
+j = 0
+reikiaSurusiuoti[j] = 
+reikiaSurusiuoti[j + 1] = 5
+tmp = reikiaSurusiuoti[j] 54
+*/
 
 
+//var reikiaSurusiuoti = [54, 5, 8, 1, -1, -185, 45, 652, 1, 0, 652, -7];
 
-var reikiaSurusiuoti = [54, 5, 8, 1, -1, -185, 45, 652, 1, 0, 652, -7];
-console.log(reikiaSurusiuoti); // didejimo tvarka
-console.log(reikiaSurusiuoti); // mazejimo tvarka
+//for (var i = 0; i < reikiaSurusiuoti.length; i++) {
+//   for (var j = 0; j < reikiaSurusiuoti.length; j++) {
+//       if (reikiaSurusiuoti[j] > reikiaSurusiuoti[j + 1]) {
+//           var tmp = reikiaSurusiuoti[j];
+//           reikiaSurusiuoti[j] = reikiaSurusiuoti[j + 1];
+//           reikiaSurusiuoti[j + 1] = tmp;
+//       }
+//   }
+//}
+//console.log(reikiaSurusiuoti); // didejimo tvarka
 
+//for (var i = 0; i < reikiaSurusiuoti.length; i++) {
+//   for (var j = 0; j < reikiaSurusiuoti.length; j++) {
+//       if (reikiaSurusiuoti[j] < reikiaSurusiuoti[j + 1]) {
+//           var tmp = reikiaSurusiuoti[j];
+//           reikiaSurusiuoti[j] = reikiaSurusiuoti[j + 1];
+//           reikiaSurusiuoti[j + 1] = tmp;
+//       }
+//   }
+//}
+//console.log(reikiaSurusiuoti); // mazejimo tvarka
