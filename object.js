08fa42cc661a7c91106369d274c24cb9b15f21e5
DmitrijKo <kdimka666@gmail.com>
2021-07-20 21:05:38 +0300

    07-20

--- object.js
+++ object.js
@@ -33,7 +33,7 @@ p.spalva = 'melynas'
 //console.log(kitasPuodukas);
 
 
-///
+/// ---------- ///
 
 
 let namas = {
@@ -52,7 +52,9 @@ n.spalva = 'geltonas';
 
 //console.log(kitasNamas);
 
-///
+
+/// ---------- ///
+
 
 let namas1 = {
    aukstai: 4,
@@ -70,3 +72,41 @@ let a = namas1;
 //console.log(namas1 == n);  // true
 //console.log(namas2 == n);  // false
 
+
+/// ---------- ///
+
+
+let jonas = {
+   vardas: "Jonas",
+   pavarde: "Jonaitis"
+};
+
+jonas.adresai = [{
+      addr: "Gedimino 9",
+      zmogus: jonas
+   },
+   {
+      addr: "Sauletekio 117",
+      zmogus: jonas
+   }
+];
+
+let petras = {
+   vardas: "Petras",
+   pavarde: "Petraitis"
+}
+
+petras.adresai = [{
+   addr: "Savanoriu 1",
+   zmogus: petras
+}];
+
+console.log(petras);
+
+let adresas = petras.adresai[0];
+
+console.log(adresas.zmogus.vardas + " gyvena " + adresas.addr);
+
+adresas.zmogus.vardas = "Antanas";
+
+console.log(petras.vardas, petras.pavarde);
