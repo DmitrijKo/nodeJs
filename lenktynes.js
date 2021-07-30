c8e1a9fb0fd6a59007b77361d84fb50e4c1ab769
DmitrijKo <kdimka666@gmail.com>
2021-07-30 14:03:17 +0300

    07-30

--- lenktynes.js
+++ lenktynes.js
@@ -14,3 +14,68 @@ pabaigoj atspausdinam visa turnyrine lentele isrusiuota pagal nuvaziuota kelia m
 komentatorius
 kas 100 km reikia atspausdinti lyderio pavadinima ir nuvaziuota atstuma
 */
+
+
+/// ==== LENKTYNES ==== ///
+class Masina {
+
+   constructor(pavadinimas, maxGreitis) {
+      this.pavadinimas = pavadinimas;
+      this.maxGreitis = maxGreitis;
+      this.greitis = 0;
+      this.atstumas = 0;
+   }
+
+   gazas(kiek) { 
+
+      if (kiek > this.maxGreitis) {
+         this.greitis = this.maxGreitis;
+      }
+   
+      if (this.greitis + kiek < 0) {
+         this.greitis = 0;
+      } else {
+         this.greitis += kiek;
+      }
+   }
+
+   vaziuojam() {
+      this.atstumas += this.greitis;
+   }
+}
+
+class SportineMasina extends Masina {
+
+   constructor(pavadinimas, maxGreitis) {
+      super(pavadinimas, maxGreitis);
+      this.spoileris = false;
+   }
+
+   pakeiskSpoilerioPozicija() {
+      let rand = Math.random();
+      if (rand >= 0.49) {
+          this.spoileris = true;
+      } else {
+          this.spoileris = false;
+      }
+   }
+
+}
+
+let m1 = new Masina('Volvo', 220);
+let m2 = new Masina('BMW M5', 250);
+let m3 = new Masina('Opel', 200);
+let m4 = new Masina('Mercedes-Benz', 260);
+let m5 = new Masina('Ford', 230);
+let m6 = new Masina('Peugeot', 220);
+let m7 = new SportineMasina('Bugatti', 350);
+let m8 = new SportineMasina('Porshe', 330);
+
+let masinuMasyvas = [];
+
+m8.gazas(100);
+m8.gazas(-101);
+m8.gazas(50);
+m8.gazas(-52);
+
+console.log(m8);
---
+++ test2.js
@@ -0,0 +1,9 @@
+let rand = Math.random();
+//console.log(rand);
+
+let nuo = -5;
+let iki = 5;
+let rand2 = Math.trunc(Math.random() * (iki - nuo + 1) + nuo);
+//console.log(rand2);
+
+console.log(100 - 100 == true);         
