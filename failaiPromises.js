0867ca35cd628f07a61d8cfde6f788ba2474b1c4
DmitrijKo <kdimka666@gmail.com>
2021-08-04 14:54:14 +0300

    namu darbai Promise a,b,c 08-04

--- failaiPromises.js
+++ failaiPromises.js
@@ -1,74 +1,51 @@
-const fs = require("fs/promises");
-
-const fileNameA = "./txtFailai/a.txt";
-const fileNameB = "./txtFailai/b.txt";
-const fileNameC = "./txtFailai/c.txt";
-let pilnasTextas = '';
-
-fs.readFile(fileNameA, {
-    encoding: "utf-8"
-})
-.then(
-    data => {
-        console.log("perskaiciau is failo " + fileNameA); 
-        pilnasTextas += data;
-    }
-)
-
-fs.readFile(fileNameB, {
-   encoding: "utf-8"
-})
-.then(
-   data => {
-       console.log("perskaiciau is failo " + fileNameB); 
-       pilnasTextas += data;
-   }
-)
-
-fs.readFile(fileNameC, {
-   encoding: "utf-8"
-})
-.then(
-   data => {
-       console.log("perskaiciau is failo " + fileNameC); 
-       pilnasTextas += data;
-       console.log('Surinktas tekstas:', pilnasTextas);
-   }
-)
-
-
-
-
-//.catch(
-//    err => {
-//        console.log("klaida skaitant is failo " + fileNameA);
-//        console.log(err);
-//    }
-//);
-
-
-
-
-//fs.readFile(fileNameA, {
-//    encoding: "utf-8"
-//})
-//.then(
-//    data => {
-//        console.log("perskaiciau is failo " + fileNameA);
-//        pilnasTextas.push(data) 
-//        console.log(pilnasTextas.toString());
-//    }
-//)
-//.catch(
-//    err => {
-//        console.log("klaida skaitant is failo " + fileNameA);
-//        console.log(err);
-//    }
-//);
-
-
 /*
 reikia kad i ekrana atspausdintu:
 failo a.txt turini + failo b.txt + failo c.txt turini
 
 */
+
+const fs = require("fs/promises");
+
+const fileNameA = "./txtFailai/a.txt";
+const fileNameB = "./txtFailai/b.txt";
+const fileNameC = "./txtFailai/c.txt";
+let pilnasTextas = [];
+
+fs.readFile(fileNameA, {
+      encoding: "utf-8"
+   })
+   .then(
+      data => {
+         console.log("perskaiciau is failo " + fileNameA);
+         pilnasTextas.push(data);
+         return fs.readFile(fileNameB, {
+            encoding: "utf-8"
+         });
+      }
+   )
+
+   .then(
+      data => {
+         console.log("perskaiciau is failo " + fileNameB);
+         pilnasTextas.push(data);
+         return fs.readFile(fileNameC, {
+            encoding: "utf-8"
+         });
+      }
+   )
+
+   .then(
+      data => {
+         console.log("perskaiciau is failo " + fileNameC);
+         pilnasTextas.push(data);
+         console.log('Surinktas tekstas:', pilnasTextas.toString());
+      }
+   )
+
+   .catch(
+      err => {
+         pilnasTextas = [];
+         pilnasTextas.push('Gavome klaida ' + err);
+         console.log(pilnasTextas.toString());
+      }
+   );
