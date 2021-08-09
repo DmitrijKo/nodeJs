dd2008a375f0088753b7e1b6e52e9426e1520392
DmitrijKo <kdimka666@gmail.com>
2021-08-09 23:15:28 +0300

    kartojimas 08-09

--- zmones.js
+++ zmones.js
@@ -28,43 +28,135 @@ function inputNumber(msg) {
    });
 }
 
+function displayText() {
+   console.log("1. Atspausdinti visus zmones");
+   console.log("2. Prideti nauja zmogu");
+   console.log("3. Istrinti zmogu");
+   console.log("4. Turtuoliu sarsas");
+   console.log("0. Pabaigti");
+}
+
+let zmones = [];
+
 async function main() {
    // ND
+   displayText()
 
-   // let vardas = await inputText("Ivesk varda: ");
-   // console.log(vardas);
+   try {
+      let menu = await inputNumber("Pasirink numeri: ");
 
-   // try {
-   //     let alga = await inputNumber("Ivesk alga: ");
-   //     console.log(alga);
-   // } catch (err) {
-   //     console.log("blogas skaicius", err);
-   // }
+      if (menu === 1) {
+         let allData = await fs.readFile("zmones.json", {
+            encoding: "utf-8"
+         });
 
-   // let zmones = [
-   //     {
-   //         vardas: "Jonas",
-   //         pavarde: "Jonaitis",
-   //         alga: 123.48
-   //     },
-   //     {
-   //         vardas: "Petras",
-   //         pavarde: "Petraitis",
-   //         alga: 123.48
-   //     },
-   //     {
-   //         vardas: "Antanas",
-   //         pavarde: "Antanaitis",
-   //         alga: 123.48
-   //     }
-   // ];
-   // try {
-   //     await fs.writeFile("zmones.json", JSON.stringify(zmones, null, 2), {
-   //         encoding: "utf-8"
-   //     });
-   // } catch (err) {
-   //     console.log("Failed to write to file", err);
-   // }
+         let zmObj = JSON.parse(allData)
+
+         console.log("Visu zmoniu sarasas:");
+
+         for (let i = 0; i < zmObj.length; i++) {
+            console.log(`${i + 1}. ${zmObj[i].vardas} ${zmObj[i].pavarde} ${zmObj[i].alga}`);
+         }
+      }
+
+      if (menu === 2) {
+         let allData = await fs.readFile("zmones.json", {
+            encoding: "utf-8"
+         })
+
+         let zmObj = JSON.parse(allData)
+         //console.log(zmObj);
+         let vardas = await inputText("Ivesk varda: ");
+         let pavarde = await inputText("Ivesk pavarde: ");
+         try {
+            let alga = await inputNumber("Ivesk alga: ");
+
+            let newObj = {
+               vardas,
+               pavarde,
+               alga
+            };
+
+            zmObj.push(newObj);
+
+            console.log("Naujas sarasas: ");
+            for (let i = 0; i < zmObj.length; i++) {
+               console.log(`${i + 1}. ${zmObj[i].vardas} ${zmObj[i].pavarde} ${zmObj[i].alga}`);
+            }
+
+            try {
+               await fs.writeFile("zmones.json", JSON.stringify(zmObj, null, 4), {
+                  encoding: "utf-8"
+               });
+            } catch (err) {
+               console.log("Failed to write to file", err);
+            }
+
+         } catch (err) {
+            console.log("blogas skaicius", err);
+         }
+      }
+
+      if (menu === 3) {
+         try {
+            let allData = await fs.readFile("zmones.json", {
+               encoding: "utf-8"
+            });
+   
+            let zmObj = JSON.parse(allData);
+
+            let numeris = await inputNumber("Ivesk numeri: ");
+
+            let delNumber = numeris - 1;
+            zmObj.splice(delNumber, 1);
+
+            console.log("Naujas sarasas: ");
+            for (let i = 0; i < zmObj.length; i++) {
+               console.log(`${i + 1}. ${zmObj[i].vardas} ${zmObj[i].pavarde} ${zmObj[i].alga}`);
+            }
+
+            try {
+               await fs.writeFile("zmones.json", JSON.stringify(zmObj, null, 4), {
+                  encoding: "utf-8"
+               });
+            } catch (err) {
+               console.log("Failed to write to file", err);
+            }
+
+         } catch (err) {
+            console.log("blogas numeris", err);
+         }
+      }
+
+      if (menu === 4) {
+         try {
+            let allData = await fs.readFile("zmones.json", {
+               encoding: "utf-8"
+            });
+   
+            let zmObj = JSON.parse(allData);
+            
+            let alga = await inputNumber("Turtuolio alga: ");
+            console.log("Turtuoliu sarasas:");
+
+            zmObj.forEach(el => {
+               if (el.alga > alga) {
+                  console.log(el.vardas, el.pavarde, el.alga);
+               }
+            });
+         }
+         catch (err) {
+            console.log("Tai ne skaicius:", err);
+         }
+      }
+
+      if (menu === 0) {
+         rl.close();
+      }
+
+   } catch (err) {
+      throw new Error("Klaida, ivestas neteisingas skaicius", err)
+   }
 
    rl.close();
 }
@@ -90,17 +182,18 @@ jei failo nera - programa sukuria faila pirmo pridejimo metu
 
 1. perskaitom is zmones.json ir parodom sarasa (su numeriais - eiles numeris masyve)
 jei failo nera - nieko nespausdinam
+
 2. papraso ivesti varda, pavarde, alga
 prideda nauja zmogu i sarasa (prie failo, jei nera sukuria)
+
 3. papraso ivesti numeri (is saraso) zmogaus, kuri reikia istrinti
-istrina is saraso ir perraso faila
-0. baigia darba
+istrina is saraso ir perraso faila.
 
-pasirinkus neegzistuojanti meniu punkta (jei ivede ne 0, 1, 2, 3, tai vel spausdinti meniu)
-
-*)
 4. turtuoliu sarsas
 papraso ivesti skaiciu
 atspausdina visus zmones, kuriu alga didesne uz ivesta skaiciu
 
+0. baigia darba
+
+pasirinkus neegzistuojanti meniu punkta (jei ivede ne 0, 1, 2, 3, tai vel spausdinti meniu)
 */
